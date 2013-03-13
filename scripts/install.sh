d30ce57136c105460dd1a43a2c271f0b1b155094
Stuart Watson <stuart.watson@unep-wcmc.org>
2013-03-13 12:59:57 +0000

    First commit

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git
+cd /usr/local/src
+wget http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise-1.8.7-2012.02.tar.gz
+tar -xvzf ruby-enterprise-1.8.7-2012.02.tar.gz
+cd /usr/local/src/ruby-enterprise-1.8.7-2012.02/
+./installer --no-tcmalloc --dont-install-useful-gems --no-dev-docs --auto /usr/local
+rm /usr/local/src/ruby-enterprise-1.8.7-2012.02.tar.gz
+/usr/local/bin/gem install chef ruby-shadow --no-ri --no-rdoc
+sudo git clone https://github.com/strtwtsn/Online_Reporting_Tool_Install.git /var/chef
+/usr/local/bin/chef-solo -c /var/chef/config/chefsolo.rb -j /var/chef/roles/ppedb.json
